ed6524f6b2c5a756f9254a77c0c621f8d84bdfc9
Juan Cruz Castro <93845022+JuanC2912@users.noreply.github.com>
2023-09-19 21:22:20 -0300

    empezando la calculadora

---
+++ Calcu/main.py
@@ -0,0 +1,6 @@
+from operaciones import *
+
+a = 2 
+b = 3
+
+print(suma(a,b))
---
+++ Calcu/operaciones.py
@@ -0,0 +1,18 @@
+
+
+def suma(a , b):
+    return a + b
+
+def resta(a , b):
+    return a - b
+
+def multiplicacion ( a , b):
+    return a * b
+
+def division (a , b):
+    if b == 0:
+        return "Error: implosible dividir por 0"
+    return a/b
+
+def exponencial (a , b):
+    return a ** b
